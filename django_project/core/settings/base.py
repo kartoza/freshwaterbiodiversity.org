328ea520531b7c18e55d2263a846d68bd60284e4
Dimas <dimas.ciputra@gmail.com>
2018-02-14 13:56:20 +0700

    Fix admin page

--- django_project/core/settings/base.py
+++ django_project/core/settings/base.py
@@ -78,6 +78,21 @@ STATICFILES_FINDERS = (
 # noinspection PyUnresolvedReferences
 from .secret import SECRET_KEY  # noqa
 
+INSTALLED_APPS = [
+    # Django apps
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.humanize',
+    'django.contrib.sitemaps',
+    'django.contrib.syndication',
+    'django.contrib.staticfiles',
+]
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -85,52 +100,32 @@ TEMPLATES = [
             # project level templates
             absolute_path('core', 'base_templates'),
         ],
-        'APP_DIRS': False,
+        'APP_DIRS': True,
         'OPTIONS': {
-            # List of callables that know how to import templates from various
-            # sources.
             'context_processors': [
-                # Already defined Django-related contexts
-                'django.contrib.auth.context_processors.auth',
-                'django.core.context_processors.request',
-                'django.core.context_processors.media',
-                'django.template.context_processors.i18n',
-
-                # `allauth` needs this from django
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-)
+]
 
 ROOT_URLCONF = 'core.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'core.wsgi.application'
-
-INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sites',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.admin',
-    'django.contrib.syndication',
-    'django.contrib.gis',
-)
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
