79fb76a5841d52e04aed00300c9a110e6d010c7d
ann26 <anita@kartoza.com>
2018-02-22 14:15:23 +0700

    add social auth

--- django_project/core/settings/contrib.py
+++ django_project/core/settings/contrib.py
@@ -8,3 +8,33 @@ STOP_WORDS = (
     'a', 'an', 'and', 'if', 'is', 'the', 'in', 'i', 'you', 'other',
     'this', 'that', 'to',
 )
+
+# Django-allauth related settings
+AUTHENTICATION_BACKENDS = (
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+INSTALLED_APPS += (
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.github',
+)
+
+SOCIALACCOUNT_PROVIDERS = {
+    'github': {
+        'SCOPE': ['user:email', 'public_repo', 'read:org']
+    }
+}
+
+ACCOUNT_UNIQUE_EMAIL = True
+ACCOUNT_USERNAME_REQUIRED = True
+ACCOUNT_EMAIL_REQUIRED = True
+# ACCOUNT_SIGNUP_FORM_CLASS = 'base.forms.SignupForm'
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+LOGIN_REDIRECT_URL = "/"
