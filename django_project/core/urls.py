832fa4beafbf2de498f789cb3ad6b6ec380776d4
meomancer <meomancer@gmail.com>
2018-02-22 10:23:54 +0700

    remove tests (is not needed for now)

--- django_project/core/urls.py
+++ django_project/core/urls.py
@@ -12,10 +12,9 @@ Class-based views
 Including another URLconf
     1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^roles/', include('roles.urls'), name="roles")
+    url(r'^admin/', admin.site.urls)
 ]
