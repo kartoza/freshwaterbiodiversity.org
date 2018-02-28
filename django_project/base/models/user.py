4182b41178cd30ea7185bfb02213bd11112b4b32
meomancer <meomancer@gmail.com>
2018-02-28 10:58:05 +0700

    fix travis

--- django_project/base/models/user.py
+++ django_project/base/models/user.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     class Meta:
         ordering = ('first_name',)
+        app_label = 'base'
 
     def __unicode__(self):
         return u'%s %s' % (self.first_name, self.last_name)
