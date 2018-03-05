fb88b88e14addee713c7dd0c7c8632c3e1626765
Dimas <dimas.ciputra@gmail.com>
2018-03-05 13:48:34 +0700

    Show full screen map in landing page

---
+++ django_project/base/static/js/map.js
@@ -0,0 +1,25 @@
+
+var baseSourceLayer;
+
+if(bingMapKey) {
+    baseSourceLayer = new ol.source.BingMaps({
+        key: bingMapKey,
+        imagerySet: 'AerialWithLabels'
+    })
+} else {
+    baseSourceLayer = new ol.source.OSM();
+}
+
+
+var map = new ol.Map({
+    target: 'map',
+    layers: [
+        new ol.layer.Tile({
+            source: baseSourceLayer
+        })
+    ],
+    view: new ol.View({
+        center: ol.proj.fromLonLat([22.937506, -30.559482]),
+        zoom: 7
+    })
+});
--- django_project/base/urls.py
+++ django_project/base/urls.py
@@ -2,7 +2,9 @@
 
 from django.urls import path
 from django.views.generic import TemplateView
+from base.views.landing_page import LandingPageView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="landing_page.html"))
+    path('', TemplateView.as_view(template_name="landing_page.html")),
+    path('map', LandingPageView.as_view()),
 ]
