f23687d1d0ea7dd960fea710c2c6af7012903646
Dimas <dimas.ciputra@gmail.com>
2018-03-06 11:15:51 +0700

    Add map control panel

--- django_project/base/static/js/map.js
+++ django_project/base/static/js/map.js
@@ -21,6 +21,9 @@ var map = new ol.Map({
     view: new ol.View({
         center: ol.proj.fromLonLat([22.937506, -30.559482]),
         zoom: 7
+    }),
+    controls: ol.control.defaults({
+        zoom: false
     })
 });
 
@@ -29,4 +32,22 @@ $('.close-panel').click(function () {
     $( ".right-panel" ).toggle( "slide", {
         direction: 'right'
     });
+});
+
+$('.zoom-in').click(function () {
+    var view = map.getView();
+    var zoom = view.getZoom();
+    view.animate({
+        zoom: zoom-1,
+        duration: 250
+    })
+});
+
+$('.zoom-out').click(function () {
+    var view = map.getView();
+    var zoom = view.getZoom();
+    view.animate({
+        zoom: zoom+1,
+        duration: 250
+    })
 });
