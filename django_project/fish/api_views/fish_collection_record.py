c3d9dd31e81bec177a16710080308de901eb09bf
Dimas <dimas.ciputra@gmail.com>
2018-03-13 13:32:32 +0700

    Add api for fish collection and taxon

--- django_project/fish/api_views/fish_collection_record.py
+++ django_project/fish/api_views/fish_collection_record.py
@@ -1,4 +1,5 @@
 # coding=utf8
+from django.http import Http404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from fish.models.fish_collection_record import FishCollectionRecord
@@ -8,21 +9,27 @@ from fish.serializers.fish_collection_serializer import \
 
 class FishCollectionList(APIView):
     """
-    List all fish collection
+    List all fish collection.
     """
 
     def get(self, request, *args):
-        """Get the collection records.
-
-        :param request: HTTP request object
-        :type request: HttpRequest
-
-        :param args: Positional arguments
-        :type args: tuple
-
-        :returns: URL
-        :rtype: HttpResponse
-        """
         fish_collections = FishCollectionRecord.objects.all()
         serializer = FishCollectionSerializer(fish_collections, many=True)
+        return Response(serializer.data)
+
+
+class FishCollectionDetail(APIView):
+    """
+    Retrieve a fish collection instance.
+    """
+
+    def get_object(self, pk):
+        try:
+            return FishCollectionRecord.objects.get(pk=pk)
+        except FishCollectionRecord.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        fish_collection = self.get_object(pk)
+        serializer = FishCollectionSerializer(fish_collection)
         return Response(serializer.data)
