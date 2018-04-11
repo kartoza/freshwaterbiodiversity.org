8abf6ee580fa0958587df4af9154bd9ec734fd5d
Dimas <dimas.ciputra@gmail.com>
2018-04-11 19:10:44 +0700

    Update icons

--- django_project/base/static/js/views/location_site.js
+++ django_project/base/static/js/views/location_site.js
@@ -21,6 +21,17 @@ define(['backbone', 'models/location_site', 'openlayers'], function (Backbone, L
                     }
                 ]
             };
+
+            var iconStyle = new ol.style.Style({
+                image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
+                    anchor: [0.5, 46],
+                    anchorXUnits: 'fraction',
+                    anchorYUnits: 'pixels',
+                    opacity: 0.75,
+                    src: 'static/img/marker.png'
+                }))
+            });
+
             var features = new ol.format.GeoJSON().readFeatures(geojson, {
                 featureProjection: 'EPSG:3857'
             });
