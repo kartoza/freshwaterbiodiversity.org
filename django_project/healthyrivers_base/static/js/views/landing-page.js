9123341a2c954e4eef69c5b15a082353a49e1698
Anita Hapsari <anita@kartoza.com>
2018-10-16 12:39:33 +0700

    fix fish summary (#256)
    * fix fish summary
    
    * fix data
    
    * clean data

--- django_project/healthyrivers_base/static/js/views/landing-page.js
+++ django_project/healthyrivers_base/static/js/views/landing-page.js
@@ -8,41 +8,50 @@ $(document).ready(function () {
         }
     });
     $.ajax({
-        url: '/api/fish-collections/',
+        url: '/api/fish-summary/',
         dataType: 'json',
         success: function (data) {
             $('#chart-fish').parent().empty().append('<canvas id="chart-fish" width="250px" height="250px"></canvas>');
-            var total_sites = Object.keys(data).length;
-            $('#fish-total-records').html(total_sites);
             var native = 0;
             var non_native = 0;
             var translocated = 0;
-            $.each(data, function (index, key) {
-                if(key['category'] === 'native' || key['category'] === 'indigenous'){
-                    native += 1;
-                }
-                if(key['category'] === 'non-native' || key['category'] === 'alien'){
-                    non_native += 1;
-                }
-                if(key['category'] === 'translocated'){
-                    translocated += 1;
-                }
 
-                var fishContainer = document.getElementById("chart-fish");
-                var fishData = {
-                    labels: ["Native", "Non-Native", "Translocated"],
-                    datasets: [{
-                        data: [native, non_native, translocated],
-                        backgroundColor: [
-                            '#a13447',
-                            '#00a99d',
-                            '#e0d43f'
-                        ],
-                        borderWidth: 1
-                    }]
-                };
-                createDonutGraph(fishContainer, fishData)
-            })
+            if(data.hasOwnProperty('total_fish')){
+                $('#fish-total-records').html(data['total_fish']);
+            }
+
+            if(data.hasOwnProperty('category')) {
+                if (data['category']['indigenous']) {
+                    native += data['category']['indigenous'];
+                }
+                if (data['category']['native']) {
+                    native += data['category']['native'];
+                }
+                if (data['category']['alien']) {
+                    non_native += data['category']['alien'];
+                }
+                if (data['category']['non-native']) {
+                    non_native += data['category']['non-native'];
+                }
+                if (data['category']['translocated']) {
+                    translocated += data['category']['translocated'];
+                }
+            }
+
+            var fishContainer = document.getElementById("chart-fish");
+            var fishData = {
+                labels: ["Native", "Non-Native", "Translocated"],
+                datasets: [{
+                    data: [native, non_native, translocated],
+                    backgroundColor: [
+                        '#a13447',
+                        '#00a99d',
+                        '#e0d43f'
+                    ],
+                    borderWidth: 1
+                }]
+            };
+            createDonutGraph(fishContainer, fishData)
         }
     })
 });
