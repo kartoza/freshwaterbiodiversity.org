8fa86b6a6ac2b617c7c50bd3aedd8b6a285d2d96
Dimas Ciputra <dimas.ciputra@gmail.com>
2019-05-15 09:24:38 +0700

    Show invert summary (#316)

--- django_project/healthyrivers_base/static/js/views/landing-page.js
+++ django_project/healthyrivers_base/static/js/views/landing-page.js
@@ -4,60 +4,94 @@ $(document).ready(function () {
         dataType: 'json',
         success: function (data) {
             var fishData = null;
-            if (data.hasOwnProperty('Fish')) {
-                fishData = data['Fish'];
-            } else if (data.hasOwnProperty('fish')) {
-                fishData = data['Fish'];
+            if (data.hasOwnProperty('fish')) {
+                fishData = data['fish'];
             }
-            if (!fishData) {
-                return;
+            createFishCharts(fishData);
+            if (data.hasOwnProperty('invert')) {
+                createInvertCharts(data['invert']);
             }
-
-            $('#chart-fish').parent().empty().append('<canvas id="chart-fish" width="150px" height="150px"></canvas>');
-            var native = 0;
-            var non_native = 0;
-            var translocated = 0;
-            var totalFish = 0;
-            var totalSite = 0;
-
-            $.each(fishData, function (fishClassName, fishClassData) {
-                if (fishClassData.hasOwnProperty('total')) {
-                    totalFish += fishClassData['total'];
-                }
-                if (fishClassData.hasOwnProperty('alien')) {
-                    non_native += fishClassData['alien'];
-                }
-                if (fishClassData.hasOwnProperty('indigenous')) {
-                    native += fishClassData['indigenous'];
-                }
-                if (fishClassData.hasOwnProperty('translocated')) {
-                    translocated += fishClassData['translocated'];
-                }
-                if (fishClassData.hasOwnProperty('total_site')) {
-                    totalSite += fishClassData['total_site'];
-                }
-            });
-
-            $('#fish-total-records').html(totalFish);
-            $('#fish-total-sites').html(totalSite);
-
-            var fishContainer = document.getElementById("chart-fish");
-            var fishData = {
-                labels: ["Native", "Non-Native", "Translocated"],
-                datasets: [{
-                    data: [native, non_native, translocated],
-                    backgroundColor: [
-                        '#a13447',
-                        '#00a99d',
-                        '#e0d43f'
-                    ],
-                    borderWidth: 1
-                }]
-            };
-            createDonutGraph(fishContainer, fishData)
         }
     })
 });
+
+function createFishCharts(fishData) {
+    if (!fishData) {
+        return;
+    }
+    $('#chart-fish').parent().empty().append('<canvas id="chart-fish" width="150px" height="150px"></canvas>');
+    var native = 0;
+    var non_native = 0;
+    var translocated = 0;
+    var totalFish = 0;
+    var totalSite = 0;
+
+    if (fishData.hasOwnProperty('total')) {
+        totalFish += fishData['total'];
+    }
+    if (fishData.hasOwnProperty('alien')) {
+        non_native += fishData['alien'];
+    }
+    if (fishData.hasOwnProperty('indigenous')) {
+        native += fishData['indigenous'];
+    }
+    if (fishData.hasOwnProperty('translocated')) {
+        translocated += fishData['translocated'];
+    }
+    if (fishData.hasOwnProperty('total_site')) {
+        totalSite += fishData['total_site'];
+    }
+
+    $('#fish-total-records').html(totalFish);
+    $('#fish-total-sites').html(totalSite);
+
+    var fishContainer = document.getElementById("chart-fish");
+    var fishChartDataset = {
+        labels: ["Native", "Non-Native", "Translocated"],
+        datasets: [{
+            data: [native, non_native, translocated],
+            backgroundColor: [
+                '#a13447',
+                '#00a99d',
+                '#e0d43f'
+            ],
+            borderWidth: 1
+        }]
+    };
+    createDonutGraph(fishContainer, fishChartDataset)
+}
+
+function createInvertCharts(invertData) {
+     $('#chart-invert').parent().empty().append('<canvas id="chart-invert" width="150px" height="150px"></canvas>');
+    let invertContainer = document.getElementById("chart-invert");
+    let totalInvert = 0;
+    let totalSite = 0;
+    if (invertData.hasOwnProperty('total')) {
+        totalInvert += invertData['total'];
+    }
+    if (invertData.hasOwnProperty('total_site')) {
+        totalSite += invertData['total_site'];
+    }
+    $('#invert-total-records').html(totalInvert);
+    $('#invert-total-sites').html(totalSite);
+    let labels = [];
+    let backgroundColors = [];
+    let chartData = [];
+    $.each(invertData['ecological_data'], function (index, ecologicalData) {
+        labels.push(ecologicalData['value']);
+        backgroundColors.push(ecologicalData['color']);
+        chartData.push(ecologicalData['count']);
+    });
+    let invertChartDataset = {
+        labels: labels,
+        datasets: [{
+            data: chartData,
+            backgroundColor: backgroundColors,
+            borderWidth: 1
+        }]
+    };
+    createDonutGraph(invertContainer, invertChartDataset);
+}
 
 function createDonutGraph(container, data) {
     var donutChart = new Chart(container, {
